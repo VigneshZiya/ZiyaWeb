9aa7d12f71e5981e2786738bf17f45773c0c81b5
Vignesh <vignesh@ziyaacademy.co.in>
2025-09-27 17:04:01 +0530

    Update mailConfig.js

--- server/config/mailConfig.js
+++ server/config/mailConfig.js
@@ -3,14 +3,11 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   host: 'smtp.gmail.com',
-  port: 465,
+  port: 587,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },
- tls: {
-    rejectUnauthorized: false, // helps avoid self-signed cert issues
   },
 });
 
