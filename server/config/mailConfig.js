8e9261cb8ea77a21cc417f07db96f97b78e13369
Vignesh <vignesh@ziyaacademy.co.in>
2025-09-27 12:30:15 +0530

    Update mailConfig.js
    port change

--- server/config/mailConfig.js
+++ server/config/mailConfig.js
@@ -3,12 +3,16 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000, // wait up to 10 seconds to connect
+  socketTimeout: 10000,      // wait up to 10 seconds for data
+  tls: {
+    rejectUnauthorized: false, // helps avoid self-signed cert issues
 });
 
 // Verify connection configuration
